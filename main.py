2c001eaaf69f72f42fcdee27cbfc8378f84d1530
Arav Bhattacharya <bhattacharya.arav05@gmail.com>
2026-02-10 12:56:35 +0000

    Add CLI

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 import pandas
 from election import run_election
 from fraudCheck import nameCheck
+import argparse
 
-# --- SETTINGS ---
-fraudProtection = True
-fileName = "Anonymized_MT25.csv"
-beginningColumnIndex = 5
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--filename',type=str,help='name of election file',default='Anonymized.csv')
+    parser.add_argument('--fraud_protect',type=bool,help='whether fraud protection is enabled',default=True)
+    parser.add_argument('-c','--col_start',type=int,help='column of first election',default=6)
 
-# ----------------
+    args = parser.parse_args()
 
+    fileName = args.filename
+    fraudProtection = args.fraud_protect
+    beginningColumnIndex = args.col_start
 
-resultsDF = pandas.read_csv(fileName)
-#do identity checks here:
+    resultsDF = pandas.read_csv(fileName)
+    #do identity checks here:
 
-if fraudProtection:
-    protectedDF = nameCheck(resultsDF)
-else:
-    protectedDF = resultsDF
+    if fraudProtection:
+        protectedDF = nameCheck(resultsDF)
+    else:
+        protectedDF = resultsDF
 
-#need to extract just the vote tables from the csv
-#should be the same amount of added fields every time?
-numCols = len(protectedDF.columns)
-votesDF = protectedDF.iloc[:, beginningColumnIndex:numCols:1]
-
-
-#print(protectedDF)
-#print(protectedDF.iloc[:, 5:numCols:1])
-for series_name, series in votesDF.items():
-    print(series_name.rstrip())
-    run_election(series_name.rstrip(), series)
+    #need to extract just the vote tables from the csv
+    #should be the same amount of added fields every time?
+    numCols = len(protectedDF.columns)
+    votesDF = protectedDF.iloc[:, beginningColumnIndex:numCols:1]
 
 
-#print(resultsDF.to_string)
+    #print(protectedDF)
+    #print(protectedDF.iloc[:, 5:numCols:1])
+    for series_name, series in votesDF.items():
+        print(series_name.rstrip())
+        run_election(series_name.rstrip(), series)
+
+
+    #print(resultsDF.to_string)
